14900818b8ad775f6b1fca6082a289f24600bc19
Mathews Araujo <mathews.mw@gmail.com>
2026-02-25 20:26:22 -0400

    Initial setup and tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-	return (
-		<div>
-			<h1>Prompt Manager</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Prompt Manager</h1>
+
+      <Button>Click me</Button>
+    </div>
+  );
 }
